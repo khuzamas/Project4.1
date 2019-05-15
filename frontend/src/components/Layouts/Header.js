bbb37684a46a8434cf66a492d1b26f463028e132
Alshemimri <Alshemimri@gmail.com>
2019-05-15 20:00:51 +0300

    Navbar Fixed

--- frontend/src/components/Layouts/Header.js
+++ frontend/src/components/Layouts/Header.js
@@ -4,12 +4,10 @@ import Navbar from './Navbar';
 function Header() {
   return (
     <div>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
-    <div className="container">
-    <a href="/" className="navbar-brand">HAHA</a>
+        {/* <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0"> */}
+
     <Navbar/>
-    </div>
-    </nav>
+  
     </div>
   )
 }
